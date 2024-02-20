848e285fc4615a6e4bd893043cffe06650e20b48
Ewura Ama Awere <eawere@ggc.edu>
2024-02-19 21:07:16 -0500

    second

--- src/EcoTransport/Models/BaseVehicle.java
+++ src/EcoTransport/Models/BaseVehicle.java
@@ -1,5 +1,17 @@
 package EcoTransport.Models;
 
+/**
+ * class: BaseVehicle
+ * @author: Ewura Ama Awere
+ * @version: 1.0
+ * @written on: Feb. 19th, 2024
+ * course: ITEC 2150 - Spring 2024
+ * description: The superclass BaseVehicle in the package EcoTransport.Models that determines the behavior of
+ * the three subclasses Bicycle, ElectricCar, and ElectricScooter.
+ * They each contain constructors, getters, setters, and toString methods.
+ * The toString methods in the subclasses override the one in the superclass,
+ * adding specifications unique to each different method of transportation.
+ */
 public class BaseVehicle {
     protected String ownerName;
     protected String vehicleType;
